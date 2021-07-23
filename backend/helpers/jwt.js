8cbfd28dd1d2f44f0bdf676279986928f5773f38
Mihalcea Andreea <andreea_mih97@yahoo.com>
2021-07-23 16:52:27 +0300

    statuc folder & excluding uploads folder

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -1,30 +1,34 @@
-const expressJwt = require('express-jwt');
+const expressJwt = require('express-jwt')
 
 function authJwt() {
-    const secret = process.env.secret;
-    const api = process.env.API_URL;
+    const secret = process.env.secret
+    const api = process.env.API_URL
     return expressJwt({
         secret,
         algorithms: ['HS256'],
-        isRevoked: isRevoked
+        isRevoked: isRevoked,
     }).unless({
         path: [
-            {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+            {
+                url: /\/api\/v1\/orders(.*)/,
+                methods: ['GET', 'OPTIONS', 'POST'],
+            },
             `${api}/users/login`,
             `${api}/users/register`,
-        ]
+            // { url: /(.*)/ },
+        ],
     })
 }
 
 async function isRevoked(req, payload, done) {
-    if(!payload.isAdmin) {
+    if (!payload.isAdmin) {
         done(null, true)
     }
 
-    done();
+    done()
 }
-
-
 
 module.exports = authJwt
